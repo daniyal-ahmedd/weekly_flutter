40ec5b0d78e7e913ef0badb7308d48076a2356f1
Daniyal Ahmed <daniyal.ahmedgcu@gmail.com>
2024-04-29 22:47:20 +0500

    postmid_done
    sports and shared prefs stuff done.

--- Assignments/week_8_postMid/gcu_app/lib/data/shared_prefs_helper.dart
+++ Assignments/week_8_postMid/gcu_app/lib/data/shared_prefs_helper.dart
@@ -8,12 +8,22 @@ class shared_pref_Helper {
     spref = await SharedPreferences.getInstance();
   }
 
+  int getExistingCounter() {
+    return spref.getInt("counter") ?? 1;
+  }
+
   Future writeSportsEntry(sports_entry entry) async {
     await spref.setString(entry.id.toString(), entry.toJson());
+    int existingCounter = spref.getInt("counter") ?? 1;
+    await spref.setInt("counter", existingCounter + 1);
   }
 
   sports_entry readSportsEntry(int id) {
     return sports_entry.fromJson(spref.getString(id.toString()) ?? "");
+  }
+
+  Future removeSportsEntry(String id) async {
+     await spref.remove(id);
   }
 
   List<sports_entry> getAllSportsData() {
@@ -22,9 +32,12 @@ class shared_pref_Helper {
     var keys = spref.getKeys();
 
     keys.forEach((element) {
-      var jsonString = spref.getString(element);
-      sports_entry entry = sports_entry.fromJson(jsonString ?? "");
-      list_sports_entries.add(entry);
+      if( element != "counter")
+      {
+        var jsonString = spref.getString(element);
+        sports_entry entry = sports_entry.fromJson(jsonString ?? "");
+        list_sports_entries.add(entry);
+        }
     });
 
 
--- Assignments/week_8_postMid/gcu_app/lib/screens/sports.dart
+++ Assignments/week_8_postMid/gcu_app/lib/screens/sports.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:gcu_app/data/shared_prefs_helper.dart';
 import 'package:gcu_app/data/sports_entry.dart';
-import '../common/ownNavBar.dart';
-import '../common/menudrawer.dart';
 
 class Sports extends StatefulWidget {
   const Sports({super.key});
@@ -16,7 +15,7 @@ class _SportsState extends State<Sports> {
   List<sports_entry> sportsEntries = [];
   TextEditingController dateController = TextEditingController();
   TextEditingController descController = TextEditingController();
-
+  String error = "";
 @override
   void initState() {
     // TODO: implement initState
@@ -37,7 +36,9 @@ class _SportsState extends State<Sports> {
       body: ListView(
         children: getAllData(),
       ),
-      //floatingActionButton: FloatingActionButton(onPressed: onAddSportsEntry()),
+      floatingActionButton: FloatingActionButton(
+        onPressed: onAddSportsEntry,
+        child: Icon(Icons.add_alert)),
     );
   }
 
@@ -46,9 +47,18 @@ class _SportsState extends State<Sports> {
     
     sportsEntries.forEach((element) {
       listSportsEntriesWidget.add(
-        ListTile(
-          title: Text(element.date),
-          subtitle: Text(element.description),
+        Dismissible(
+          key: UniqueKey(),
+          onDismissed: (_) {
+            sharedPrefHelper.removeSportsEntry(element.id.toString()).then((value) {
+              sportsEntries=sharedPrefHelper.getAllSportsData();
+              setState(() {});
+            });
+          },
+          child: ListTile(
+            title: Text(element.date),
+            subtitle: Text(element.description),
+          ),
         ),
       );
     });
@@ -58,24 +68,82 @@ class _SportsState extends State<Sports> {
   }
 
   Future onAddSportsEntry() async {
-        return AlertDialog(
-          title: Text("Add the sports entry here"),
-          content: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextField(
-                  controller: dateController,
-                  onTap: _selectDate,
-                  readOnly: true,
-                ),
-                TextField(
-                  controller: descController;
-                  onTap:
+        return showDialog(
+          context: context,
+          builder: (context) {
+          return StatefulBuilder( builder: (context, StateSetter stateSetter) 
+             {
+              return AlertDialog(
+              title: Text("Add the sports entry here"),
+              content: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: dateController,
+                      onTap: _selectDate,
+                      readOnly: true, //so keyborad doesen't show up when showing the date picker element.
+                      decoration: InputDecoration( hintText: "Please select Date of the match:"),
+                    ),
+                    TextField(
+                      controller: descController,
+                      decoration: InputDecoration( hintText: "Please give description of the match.")
+                    ),
+                    Text( error, style: TextStyle(color: Colors.red.shade900),)
+                  ],
                 )
+              ),
+              actions: [
+                  TextButton(
+                    onPressed: () {
+                      dateController.text = "";
+                      descController.text = "";
+                      Navigator.of(context).pop();
+                      error = "";
+                      setState(() {});
+                    } ,
+                    child: Text("Cancel"),
+                  ),
+                  ElevatedButton( 
+                    onPressed: () {
+                    String date = dateController.text;
+                    String desc = descController.text;
+            
+                    int savedCounter = sharedPrefHelper.getExistingCounter();
+            
+                    if(date != "" && desc != ""){
+                      sports_entry entry = sports_entry(savedCounter, desc, date);
+                      sharedPrefHelper.writeSportsEntry(entry);
+                      dateController.text = "";
+                      descController.text = "";
+                      Navigator.of(context).pop();
+                      sportsEntries = sharedPrefHelper.getAllSportsData();
+                      setState(() {});
+                    }
+                    else{
+                      error="Please provide appropriate information.";
+                      stateSetter( 
+                        () {}
+                      );
+                      //error = "";
+                    }
+                    },
+                    child: Text("Save"),
+                  ), 
               ],
-            )
-          )
-        )
+            );
+          });
+  });
+  }
+
+  Future _selectDate() async {
+      final DateTime? dated = await showDatePicker(context: context, 
+        firstDate: DateTime(2024), 
+        lastDate: DateTime(DateTime.now().year,DateTime.now().month + 1));
+        
+        if(dated !=null)
+        {
+          dateController.text = "${dated.day}-${dated.month}-${dated.year}";
+        }
   }
 }
 
